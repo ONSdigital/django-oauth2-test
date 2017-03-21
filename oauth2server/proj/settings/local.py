4b4baba32709d589f6b78a45c0e5028c032989ae
Charles <enquiries@movingdigi.com>
2017-03-21 15:10:12 +0000

    local.py removed from gitignore

---
+++ oauth2server/proj/settings/local.py
@@ -0,0 +1,26 @@
+import sysfrom proj.settings.default import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = 'tbd(pv7679n_w-t++*s_*oon&#v0ubhkxhzvlq51ko2+=dt*z#'
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': 'django_oauth2_server',
+       'USER': '',
+       'PASSWORD': '',
+       'HOST': '',
+   },
+}
+
+DEBUG = True
+
+OAUTH2_SERVER = {
+   'ACCESS_TOKEN_LIFETIME': 3600,
+   'AUTH_CODE_LIFETIME': 3600,
+   'REFRESH_TOKEN_LIFETIME': 3600,
+   'IGNORE_CLIENT_REQUESTED_SCOPE': False,
+}
