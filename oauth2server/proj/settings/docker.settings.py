03e3f6e886591946463c7d9962b172e9915f95a6
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-05-19 16:12:37 +0100

    first commit for this branch. it's a rip off from Marks code - not tested or changed. so will not work. just pushed to make sure it does not get lost.

--- oauth2server/proj/settings/docker.settings.py
+++ oauth2server/proj/settings/docker.settings.py
@@ -20,6 +20,30 @@ SECRET_KEY = 'tbd(pv7679n_w-t++*s_*oon&#v0ubhkxhzvlq51ko2+=dt*z#'           #TOD
 # }
 
 
+### Extract the database URI value from VCAP_SERVICES
+def getDatabaseUri():
+
+    global uri
+
+    if uri is not None:
+        return uri
+
+    if 'VCAP_SERVICES' in os.environ:
+        print('VCAP_SERVICES found in os.environ')
+        decoded_config = json.loads(os.environ['VCAP_SERVICES'])
+    else:
+        print('VCAP_SERVICES NOT found in os.environ')
+        return os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://ras_frontstage_backup:password@localhost:5431/postgres')
+
+    for key, value in decoded_config.items():
+        print('Inspecting key: "' + str(key) + '" with value: ' + str(value))
+        if decoded_config[key][0]['name'] == 'postgresql':
+            creds = decoded_config[key][0]['credentials']
+            uri = creds['uri']
+            print('Postgres DATABASE uri: ' + uri)
+            return uri
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
