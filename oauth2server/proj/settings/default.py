b78dd13899f2fcc66d8ff0440c8e1bbb691232ac
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-08-17 17:04:25 +0100

    removing analytics app which is not in this branch

--- oauth2server/proj/settings/default.py
+++ oauth2server/proj/settings/default.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.analytics',
     'apps.accounts',
     'apps.credentials',
     'apps.information',
