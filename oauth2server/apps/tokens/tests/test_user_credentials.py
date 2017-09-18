d36e7be402e92015e897705513544c069fb5daa6
Andrew Millar <andrew.millar@qa.com>
2017-09-18 15:16:47 +0100

    verifying user details before locked/verified

--- oauth2server/apps/tokens/tests/test_user_credentials.py
+++ oauth2server/apps/tokens/tests/test_user_credentials.py
@@ -3,12 +3,9 @@ import base64
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.test import TestCase
-from django.conf import settings
 
-from apps.tokens.models import (
-    OAuthAccessToken,
-    OAuthRefreshToken,
-)
+from apps.credentials.models import OAuthUser
+from apps.tokens.models import OAuthAccessToken, OAuthRefreshToken
 
 
 class UserCredentialsTest(TestCase):
@@ -20,11 +17,10 @@ class UserCredentialsTest(TestCase):
 
     def setUp(self):
         self.api_client = APIClient()
-
-    def test_missing_client_credentials(self):
         self.assertEqual(OAuthAccessToken.objects.count(), 0)
         self.assertEqual(OAuthRefreshToken.objects.count(), 0)
 
+    def test_missing_client_credentials(self):
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -36,16 +32,10 @@ class UserCredentialsTest(TestCase):
 
         self.assertEqual(OAuthAccessToken.objects.count(), 0)
         self.assertEqual(OAuthRefreshToken.objects.count(), 0)
-
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data['detail'], u'Client credentials were not found in the headers or body')
-        #self.assertEqual(response.data['error_description'], u'Client credentials were not found in the headers or body')
-
 
     def test_invalid_client_credentials(self):
-        self.assertEqual(OAuthAccessToken.objects.count(), 0)
-        self.assertEqual(OAuthRefreshToken.objects.count(), 0)
-
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -62,13 +52,8 @@ class UserCredentialsTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data['detail'], u'Invalid client credentials')
-        #self.assertEqual(response.data['error_description'], u'Invalid client credentials')
-
 
     def test_corrupt_client_credentials(self):
-        self.assertEqual(OAuthAccessToken.objects.count(), 0)
-        self.assertEqual(OAuthRefreshToken.objects.count(), 0)
-
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -85,14 +70,47 @@ class UserCredentialsTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data['detail'], u'Invalid client credentials')
-        #self.assertEqual(response.data['error_description'], u'Invalid client credentials')
 
+    # This uses a user that does not exist in the database
+    def test_account_does_not_exist(self):
+        response = self.api_client.post(
+            path='/api/v1/tokens/',
+            data={
+                'grant_type': 'password',
+                'username': 'wrong@user.com',
+                'password': 'testpassword',
+            },
+            HTTP_AUTHORIZATION='Basic:{}'.format(
+                base64.encodestring('testclient:testpassword')),
+        )
 
-    # This uses the john2@doe.com account to test a user who has not had his account verified.
-    def test_account_not_verifieds(self):
         self.assertEqual(OAuthAccessToken.objects.count(), 0)
         self.assertEqual(OAuthRefreshToken.objects.count(), 0)
 
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], u'Unauthorized user credentials!!!')
+
+    # This uses the jogn@doe.com user with the wrong password
+    def test_account_wrong_password(self):
+        response = self.api_client.post(
+            path='/api/v1/tokens/',
+            data={
+                'grant_type': 'password',
+                'username': 'john@doe.com',
+                'password': 'wrong password',
+            },
+            HTTP_AUTHORIZATION='Basic:{}'.format(
+                base64.encodestring('testclient:testpassword')),
+        )
+
+        self.assertEqual(OAuthAccessToken.objects.count(), 0)
+        self.assertEqual(OAuthRefreshToken.objects.count(), 0)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], u'Unauthorized user credentials!!!')
+
+    # This uses the john2@doe.com account to test a user who has not had his account verified.
+    def test_account_not_verified(self):
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -110,12 +128,8 @@ class UserCredentialsTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data['detail'], u'User account not verified')
 
-
     # This uses the john3@doe.com account to test a user who has a verified account but is locked.
     def test_account_is_locked(self):
-        self.assertEqual(OAuthAccessToken.objects.count(), 0)
-        self.assertEqual(OAuthRefreshToken.objects.count(), 0)
-
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -133,17 +147,28 @@ class UserCredentialsTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data['detail'], u'User account locked')
 
+    # This uses the john3@doe.com account to test a user with wrong details who is locked
+    def test_account_is_locked_wrong_password(self):
+        response = self.api_client.post(
+            path='/api/v1/tokens/',
+            data={
+                'grant_type': 'password',
+                'username': 'john3@doe.com',
+                'password': 'wrongpassword',
+            },
+            HTTP_AUTHORIZATION='Basic:{}'.format(
+                base64.encodestring('testclient:testpassword')),
+        )
 
-
-
-
-    # This uses the john4@doe.com account has 9 failed logins. We need to test if we use the wrong password again it will
-    # lock the user account from obtaining a token.
-    def test_account_is_locked(self):
         self.assertEqual(OAuthAccessToken.objects.count(), 0)
         self.assertEqual(OAuthRefreshToken.objects.count(), 0)
 
-        # Request a token with the wrong password. Forcing failed attempts to be 10 and lock his account.
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['detail'], u'Unauthorized user credentials!!!')
+
+    # This uses the john4@doe.com account has 9 failed logins. We need to test if we use the wrong password again
+    # it will lock the user account from obtaining a token.
+    def test_account_is_locked_after_10_requests(self):
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -161,31 +186,10 @@ class UserCredentialsTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data['detail'], u'User account locked')
 
-
-        # Now use the correct password. This should still fail with user account locked.
-        response = self.api_client.post(
-            path='/api/v1/tokens/',
-            data={
-                'grant_type': 'password',
-                'username': 'john4@doe.com',
-                'password': 'testpassword',
-            },
-            HTTP_AUTHORIZATION='Basic:{}'.format(
-                base64.encodestring('testclient:testpassword')),
-        )
-
-        self.assertEqual(OAuthAccessToken.objects.count(), 0)
-        self.assertEqual(OAuthRefreshToken.objects.count(), 0)
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertEqual(response.data['detail'], u'User account locked')
-
-
+        locked_user = OAuthUser.objects.get(email='john4@doe.com')
+        self.assertEqual(locked_user.account_locked(), True)
 
     def test_success(self):
-        self.assertEqual(OAuthAccessToken.objects.count(), 0)
-        self.assertEqual(OAuthRefreshToken.objects.count(), 0)
-
         response = self.api_client.post(
             path='/api/v1/tokens/',
             data={
@@ -211,7 +215,6 @@ class UserCredentialsTest(TestCase):
         self.assertEqual(response.data['access_token'], access_token.access_token)
         self.assertEqual(response.data['expires_in'], 3600)
         self.assertEqual(response.data['token_type'], 'Bearer')
-        #self.assertEqual(response.data['scope'], 'foo bar qux')
         self.assertIn('qux', response.data['scope'])
         self.assertIn('foo', response.data['scope'])
         self.assertIn('bar', response.data['scope'])
