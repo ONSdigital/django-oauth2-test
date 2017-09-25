098f6ce8afbc9c81925396c04d9857665a9ef181
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-09-25 11:07:43 +0100

    putting port back to the docker version for running on local machiens. removing a test.py file as this branch does not have the tests written

--- oauth2server/proj/settings/local.py
+++ oauth2server/proj/settings/local.py
@@ -32,7 +32,7 @@ DATABASES = {
         'PASSWORD': 'postgres',                            # Not used for developing
         'HOST': 'localhost',                                # Set to using the postgres SQL DB within our docker container. See docker-compose.yml
                                                             # for information on this within the ras-compose project on Github for ONSDigital
-        'PORT': '5432',                                     # Set to the exposed endpoint via our docker-compose file. Normally it's 5432 for postgres
+        'PORT': '5431',                                     # Set to the exposed endpoint via our docker-compose file. Normally it's 5432 for postgres
     }
 }
 
