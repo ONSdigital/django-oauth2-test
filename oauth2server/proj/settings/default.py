1523a77d80f4bad4f482ede670d5c8393ef879d5
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-09-28 09:13:24 +0100

    Updating procfile and settings file to include the whitenoise config

--- oauth2server/proj/settings/default.py
+++ oauth2server/proj/settings/default.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,7 +81,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'proj', 'static'),
 )
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Used by whitenoise to speed up serving of static content.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
