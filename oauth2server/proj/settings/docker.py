4458926fa1118341492b722ccb72b38968446024
madeye-matt <matt@madeye.com>
2017-12-14 12:50:09 +0000

    Docker improvements (#30)
    * Added a django configuration for running in docker
    
    * Changed host to ras-postgres
    
    The other RAS services use ras-postgres as the database hostname, so
    changed oauth2-service to match
    
    * Logging now correctly indicates docker config used
    
    Incorporated @andrewmil suggestion for improving the logging
    
    * Added a django configuration for running in docker
    
    Changed host to ras-postgres
    
    The other RAS services use ras-postgres as the database hostname, so
    changed oauth2-service to match
    
    Logging now correctly indicates docker config used
    
    Incorporated @andrewmil suggestion for improving the logging

---
+++ oauth2server/proj/settings/docker.py
@@ -0,0 +1,35 @@
+from proj.settings.default import *
+
+
+# Standard instance of a logger with __name__. We are using this so that our root folder has our logger defined.
+stdlogger = logging.getLogger(__name__)
+stdlogger.info("*** Docker settings are being used. ***")
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'change-me'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',                                  # Or path to database file if using sqlite3.
+        'USER': 'postgres',                                  # Not used with sqlite3.
+        'PASSWORD': 'postgres',                              # Not used for developing
+        'HOST': 'ras-postgres',                                  # Set to using the postgres SQL DB within our docker container. See docker-compose.yml
+        'PORT': '5432',                                      # Set to the exposed endpoint via our docker-compose file
+    }
+}
+
+DEBUG = True                                                # Set to false on Production
+
+OAUTH2_SERVER = {
+    'ACCESS_TOKEN_LIFETIME': 3600,
+    'AUTH_CODE_LIFETIME': 3600,
+    'REFRESH_TOKEN_LIFETIME': 3600,
+    'IGNORE_CLIENT_REQUESTED_SCOPE': False,
+    'SET_AUTH_USER_ACTIVE_ON_REGISTRATION': False,           # when a new oauth user is added they are set as active by default if flag is set as true
+
+}
+
+# This defined the max number of failed logins a user can have before the account is locked
+MAX_FAILED_LOGINS = 10
