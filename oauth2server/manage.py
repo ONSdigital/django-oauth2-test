785cb652a1ae0abed53bf593b1ae77f7751ef197
Ben Jefferies <benjefferies@echosoft.uk>
2018-04-30 11:37:03 +0100

    Exit shell script on error (#34)
    * Exit shell script on error
    
    This will cause docker compose to restart the docker container if it
    fails to set up the database schema/tables
    
    * Move check for super user environmental variables to python script
    
    * Add env variables to travis

--- oauth2server/manage.py
+++ oauth2server/manage.py
@@ -2,8 +2,18 @@
 import os
 import sys
 
+
+def check_env(env):
+    if not os.getenv(env):
+        print('Missing environmental variable ' + env)
+        exit(1)
+
+
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings.local')
+    check_env('OAUTH2_SUPER_USER')
+    check_env('OAUTH2_SUPER_USER_PASSWORD')
+    check_env('OAUTH2_SUPER_USER_EMAIL')
 
     from django.core.management import execute_from_command_line
 
