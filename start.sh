db55eccf912709e21934d115f786dd5b164547e7
Jamie Cox <me@jcox.tech>
2018-05-21 13:58:09 +0100

    Test without fixtures

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 ./init_db.sh
-python oauth2server/manage.py loaddata oauth2server/apps/credentials/fixtures/ons_credentials.json
 python oauth2server/manage.py runserver 0.0.0.0:8040
