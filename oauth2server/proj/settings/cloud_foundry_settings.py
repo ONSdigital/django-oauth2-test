880bc96dff1c0da07da45190b41b2140812ea706
VibhorMehra <33091497+VibhorMehra@users.noreply.github.com>
2018-07-03 09:44:54 +0100

    Optimise gunicorn workers (#40)
    - Changing worker_class to `gevent`
    - Changing settings to use non-persistent DB connection. This is
    required when using gevent / async workers.

--- oauth2server/proj/settings/cloud_foundry_settings.py
+++ oauth2server/proj/settings/cloud_foundry_settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'PASSWORD':DB_PASSWORD,
         'HOST':DB_HOST,
         'PORT': '5432',
-        'CONN_MAX_AGE': 30,  # seconds for persistent connection, since Django 1.6
+        'CONN_MAX_AGE': 0,  # seconds for persistent connection, since Django 1.6
     }
 }
 
